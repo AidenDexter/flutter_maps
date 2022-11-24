a8d219d9199d772d88820f8ddd3f012aee88393a
slava-vl <slavabolotaev@gmail.com>
2022-11-24 13:43:54 +0300

    added determinePosition

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Google Maps for Dartapp',
       home: MapSample(),
     );
@@ -20,45 +20,105 @@ class MyApp extends StatelessWidget {
 }
 
 class MapSample extends StatefulWidget {
+  const MapSample({super.key});
+
   @override
   State<MapSample> createState() => MapSampleState();
 }
 
 class MapSampleState extends State<MapSample> {
-  final Geolocator _geolocator = Geolocator()..forceAndroidLocationManager;
-  Completer<GoogleMapController> _controller = Completer();
+  final _controller = Completer<GoogleMapController>();
+  late BitmapDescriptor customIcon = BitmapDescriptor.defaultMarker;
+  Marker? myPositionMarker;
+
   @override
   void initState() {
     super.initState();
+    BitmapDescriptor.fromAssetImage(const ImageConfiguration(size: Size(0.1, 0.1)), 'assets/icon.png').then((value) {
+      setState(() {
+        customIcon = value;
+        myPositionMarker = Marker(
+            markerId: const MarkerId('myPosition'), position: const LatLng(43.026885, 44.690811), icon: customIcon);
+      });
+    });
     _getCurrent();
   }
 
-  static final CameraPosition _vladikavkaz = CameraPosition(
+  // final Marker firstMarker = const Marker(
+  //     markerId: MarkerId('first'),
+  //     position: LatLng(43.02115, 44.68196),
+  //     infoWindow: InfoWindow(title: 'first', snippet: 'wow'));
+  // final Marker secondMarker = const Marker(
+  //     markerId: MarkerId('second'),
+  //     position: LatLng(43.026885, 44.690811),
+  //     infoWindow: InfoWindow(title: 'second', snippet: 'wow'));
+
+  static const CameraPosition _vladikavkaz = CameraPosition(
     target: LatLng(43.02115, 44.68196),
     zoom: 10.4746,
   );
+
+  // final Polyline polyline = const Polyline(polylineId: PolylineId('id'), points: [
+  //   LatLng(43.02115, 44.68196),
+  //   LatLng(43.026885, 44.690811),
+  // ]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-          initialCameraPosition: _vladikavkaz,
-          myLocationButtonEnabled: true,
-          mapType: MapType.hybrid,
-          onMapCreated: (GoogleMapController controller) {
-            {
-              _controller.complete(controller);
-            }
-          }),
+        // polylines: {polyline},
+        markers: {
+          if (myPositionMarker != null) myPositionMarker!,
+        },
+        initialCameraPosition: _vladikavkaz,
+        myLocationButtonEnabled: true,
+        mapType: MapType.normal,
+        onMapCreated: (controller) => _controller.complete(controller),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _getCurrent,
+        label: const Text('Curreent position'),
+      ),
     );
   }
 
   Future<void> _getCurrent() async {
-    _geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best).then((Position position) async {
+    _determinePosition().then((myPosition) async {
       final GoogleMapController controller = await _controller.future;
       controller.animateCamera(CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(position.latitude, position.longitude), zoom: 14)));
+          CameraPosition(target: LatLng(myPosition.latitude, myPosition.longitude), zoom: 14)));
+      setState(() {
+        myPositionMarker = Marker(
+            markerId: const MarkerId('myPosition'),
+            position: LatLng(myPosition.latitude, myPosition.longitude),
+            icon: customIcon);
+      });
     }).catchError((e) {
-      print(e);
+      print(e.toString());
     });
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
   }
 }
